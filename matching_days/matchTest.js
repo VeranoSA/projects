b2674cebf9bbdcdbd619c41b17057b1cd5429d14
Tebogo Seanego <50863472+VeranoSA@users.noreply.github.com>
2021-05-26 09:22:00 +0200

    matching days

--- matching_days/matchTest.js
+++ matching_days/matchTest.js
@@ -6,11 +6,7 @@ describe(' Matching Days function', function () {
         assert.equal(matchDays.match('Saturday', 'Saturday'), true);
         assert.equal(matchDays.match('Tuesday', 'Monday'), false);
     });
-});
-
-describe('Matching Days factory', function () {
-
-    it('Given two days as parameters it should return an array with days of the week and the status of the two days, match if they are the same, no match and alsoNomatch for non matching days ', function () {
+    it('should return an array with days of the week and the status of the two days, match if they are the same, no match and alsoNomatch for non matching days ', function () {
 
         var testDays = [{
                 day: 'Sunday',
