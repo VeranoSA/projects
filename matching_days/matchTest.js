ca951c56b44b7c39707d2abd664a2c3e8e7cc517
Tebogo Seanego <50863472+VeranoSA@users.noreply.github.com>
2021-05-27 11:35:34 +0200

    matching days finalised

--- matching_days/matchTest.js
+++ matching_days/matchTest.js
@@ -1,12 +1,23 @@
 describe(' Matching Days function', function () {
 
-    it('Should return true for matching days and false for non matching days given a 2 strings of days', function () {
+    it('Should return true for matching days given 2 strings of days', function () {
 
         let matchDays = MatchingDays('Day1', 'Day2');
         assert.equal(matchDays.match('Saturday', 'Saturday'), true);
-        assert.equal(matchDays.match('Tuesday', 'Monday'), false);
+        assert.equal(matchDays.match('Monday', 'Monday'), true);
+        assert.equal(matchDays.match('Thursday', 'Thursday'), true);
+        assert.equal(matchDays.match('Friday', 'Friday'), true);
+        assert.equal(matchDays.match('Sunday', 'Sunday'), true);
     });
-    it('should return an array with days of the week and the status of the two days, match if they are the same, no match and alsoNomatch for non matching days ', function () {
+    it('Should return false for matching days given 2 strings of days', function () {
+
+        let matchDays = MatchingDays('Day1', 'Day2');
+        assert.equal(matchDays.match('Saturday', 'Sunday'), false);
+        assert.equal(matchDays.match('Tuesday', 'Monday'), false);
+        assert.equal(matchDays.match('Saturday', 'Sunday'), false);
+        assert.equal(matchDays.match('Tuesday', 'Friday'), false);
+    });
+    it('should return an array with days of the week and the status of the two days, no match and alsoNomatch for non matching days ', function () {
 
         var testDays = [{
                 day: 'Sunday',
@@ -39,6 +50,44 @@ describe(' Matching Days function', function () {
         ];
 
         var testDays1 = [{
+            day: 'Sunday',
+            status: ''
+        },
+        {
+            day: 'Monday',
+            status: ''
+        },
+        {
+            day: 'Tuesday',
+            status: 'nomatch'
+        },
+        {
+            day: 'Wednesday',
+            status: ''
+        },
+        {
+            day: 'Thursday',
+            status: ''
+        },
+        {
+            day: 'Friday',
+            status: 'alsoNomatch'
+        },
+        {
+            day: 'Saturday',
+            status: ''
+        }
+    ];
+
+        
+
+        assert.deepEqual(MatchingDays('Monday', 'Thursday').days, testDays);
+        assert.deepEqual(MatchingDays('Tuesday', 'Friday').days, testDays1);
+
+    });
+    it('should return an array with days of the week and the status of the two days, match if they are the same', function () {
+
+        var testDays3 = [{
                 day: 'Sunday',
                 status: ''
             },
@@ -68,8 +117,37 @@ describe(' Matching Days function', function () {
             }
         ];
 
-        assert.deepEqual(MatchingDays('Monday', 'Thursday').days, testDays);
-        assert.deepEqual(MatchingDays('Monday', 'Monday').days, testDays1);
-
+        var testDays4 = [{
+            day: 'Sunday',
+            status: ''
+        },
+        {
+            day: 'Monday',
+            status: ''
+        },
+        {
+            day: 'Tuesday',
+            status: ''
+        },
+        {
+            day: 'Wednesday',
+            status: 'match'
+        },
+        {
+            day: 'Thursday',
+            status: ''
+        },
+        {
+            day: 'Friday',
+            status: ''
+        },
+        {
+            day: 'Saturday',
+            status: ''
+        }
+    ];
+        assert.deepEqual(MatchingDays('Monday', 'Monday').days, testDays3);
+        assert.deepEqual(MatchingDays('Wednesday', 'Wednesday').days, testDays4);
     });
+
 });
