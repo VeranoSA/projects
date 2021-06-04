794b12be70b45cb0466d2fa220760819e9c80fa3
Tebogo Seanego <50863472+VeranoSA@users.noreply.github.com>
2021-06-04 16:57:06 +0200

    shoes resized

--- Shoe_Catalogue/js/shoeDom.js
+++ Shoe_Catalogue/js/shoeDom.js
@@ -1,13 +1,59 @@
 let localShoes = JSON.parse(localStorage.getItem('shoesData'));
 
-let shoesArray = [{
+let shoesArray = [
+    {
+        brand: "Le Coq",
+        color: "Grey",
+        price: 1200,
+        id: 0,
+        qty: 10,
+        size: 5,
+        "img-link": "img/le coq grey.jpg"
+    },
+    {
+        brand: "Le Coq",
+        color: "Black",
+        price: 1200,
+        id: 0,
+        qty: 10,
+        size: 5,
+        "img-link": "img/le coq black.jpg"
+    },
+    {
+        brand: "Le Coq",
+        color: "White",
+        price: 1300,
+        id: 0,
+        qty: 10,
+        size: 5,
+        "img-link": "img/le coq white.jpg"
+    },
+    {
+        brand: "Le Coq",
+        color: "Blue",
+        price: 1200,
+        id: 0,
+        qty: 10,
+        size: 5,
+        "img-link": "img/le coq blue.jpg"
+    },
+    {
+        brand: "Le Coq",
+        color: "Green",
+        price: 1300,
+        id: 0,
+        qty: 10,
+        size: 5,
+        "img-link": "img/le coq green.jpg"
+    },
+    {
         brand: "Adidas",
         color: "Black",
         price: 999,
         id: 0,
         qty: 10,
         size: 5,
-        "img-link": "img/adidas black.jpeg"
+        "img-link": "img/adidas black.jpg"
     },
     {
         brand: "Adidas",
@@ -16,44 +62,80 @@ let shoesArray = [{
         id: 1,
         qty: 10,
         size: 5,
-        "img-link": "img/adidas white.jpeg"
+        "img-link": "img/adidas white.jpg"
     },
     {
         brand: "Adidas",
-        color: "Green",
+        color: "Red",
         price: 999,
-        id: 1,
+        id: 2,
         qty: 10,
         size: 5,
-        "img-link": "img/adidas green.jpeg"
+        "img-link": "img/adidas red.jpg"
     },
     {
         brand: "Adidas",
+        color: "Orange",
+        price: 1700,
+        id: 3,
+        qty: 10,
+        size: 5,
+        "img-link": "img/adidas orange.jpg"
+    },
+    {
+        brand: "Adidas",
+        color: "Grey",
+        price: 999,
+        id: 4,
+        qty: 10,
+        size: 5,
+        "img-link": "img/adidas grey.jpg"
+    },
+    {
+        brand: "Nike",
+        color: "Grey",
+        price: 999,
+        id: 5,
+        qty: 10,
+        size: 5,
+        "img-link": "img/nike grey.jpg"
+    },
+    {
+        brand: "Nike",
         color: "Pink",
-        price: 999,
-        id: 1,
+        price: 1500,
+        id: 6,
         qty: 10,
         size: 5,
-        "img-link": "img/adidas blue.jpeg"
+        "img-link": "img/nike pink.jpg"
     },
     {
-        brand: "Gucci",
+        brand: "Nike",
         color: "Blue",
         price: 999,
-        id: 1,
+        id: 7,
         qty: 10,
         size: 5,
-        "img-link": "img/gucci blue.jpg"
+        "img-link": "img/nike blue.jpg"
     },
     {
-        brand: "Gucci",
-        color: "Black",
-        price: 999,
-        id: 1,
+        brand: "Nike",
+        color: "Yellow",
+        price: 1500,
+        id: 8,
         qty: 10,
         size: 5,
-        "img-link": "img/gucci black.jpg"
-    }
+        "img-link": "img/nike yellow.jpg"
+    },
+    {
+        brand: "Nike",
+        color: "Black",
+        price: 999,
+        id: 9,
+        qty: 10,
+        size: 5,
+        "img-link": "img/nike black.jpg"
+    },
 ]
 let shoes;
 if (localShoes) {
