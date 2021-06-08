c22f4a54bbf77a38f25131c79c43dd016506a554
Tebogo Seanego <50863472+VeranoSA@users.noreply.github.com>
2021-06-08 10:17:54 +0200

    SHOE ADD TO CART

--- Shoe_Catalogue/js/ShoeFactory.js
+++ Shoe_Catalogue/js/ShoeFactory.js
@@ -1,6 +1,7 @@
 
 let ShoeFactory = function (shoesData) {
-
+    
+// ============================== ALLOWS US TO FILTER ================================//
     let getShoes = function (specs) {
 
         if (!specs.id) {
@@ -18,9 +19,33 @@ let ShoeFactory = function (shoesData) {
         });
         return filteredShoes;
     };
+
+
+// ============================================= ADD TO CART ====================================================== //
+    let addToCart = function (item) {
+        let tmpCart = JSON.parse(localStorage.getItem('cart')) || [];
+        let shoeToAdd = getShoes({
+            id: item
+        });
+
+
+        if (!tmpCart.some(function (shoe) {
+                return shoe.id === shoeToAdd[0].id;
+            })) {
+            if (shoeToAdd[0].qty > 0) {
+                shoeToAdd[0].qty--;
+                tmpCart.push(shoeToAdd[0]);
+                localStorage.setItem('shoesData', JSON.stringify(shoesData));
+                localStorage.setItem('cart', JSON.stringify(tmpCart));
+            };
+
+        };
+
+    };
     return {
 
         getShoes,
+        addToCart
 
     }
 
